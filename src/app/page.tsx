f3054c5385c270a255762b0ccf40337affd24a7b
Eric Sartorius <ericssartorius@gmail.com>
2024-10-14 15:31:29 +0100

    Fix darkmode class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
             <div className="flex flex-col items-center space-y-4 text-center">
               <div className="space-y-2">
                 <h1 className="text-3xl font-bold tracking-tighter text-primary sm:text-4xl md:text-5xl lg:text-6xl/none">
-                  Create Beautiful Digital Menus With Meny
+                  Create Beautiful Digital Menus
                 </h1>
                 <p className="mx-auto max-w-[700px] text-gray-500 dark:text-gray-400 md:text-xl">
                   Easily design and manage your restaurant&apos;s menu.
